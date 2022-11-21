e2177115e18668aef13ceb10e629f1faa67c1eab
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2022-11-21 15:50:17 -0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,45 @@ import '../styles/contact.css';
 
 const Contact = () => {
   return <div>
-<h1 class="sobrenos">nosso restaurante</h1>
-    </div>;
+<h1 class="sobrenos">Quem somos?</h1>
+<br></br>
+<h4 class="sobrenos">Somos um restaurante que está no mercado desde 2017 quando foi fundado. Nosso
+primeiro restaurante nasceu na cozinha de uma casa pequena no bairro de Campo
+Grande. Mesmo sendo pequeno sempre pensávamos além do que nossa realidade dizia e
+mostrava na época, tínhamos uma meta a ser cumprida. Queríamos levar o melhor prato ao
+paladar do máximo de pessoas possíveis de forma que ela se sinta aconchegante onde ela
+estiver comendo, seja em nosso restaurante como em qualquer lugar que ela deseje.</h4><br></br>
+
+<br></br>
+
+<h4 class="sobrenos">No início de tudo começamos apenas com quentinhas para almoço, dois anos depois nós
+conseguimos montar o primeiro restaurante oficial mas ainda não era o suficiente já que
+ainda faltavam bastantes passos a serem dados, desde então investimos mais e mais para
+que a nossa meta fosse alcançada.
+Atualmente, depois de muito esforço e determinação, temos três restaurantes físicos, não
+satisfeitos em apenas darem um local para vocês , nossos clientes, hoje também levamos o
+nosso melhor produto até a mesa da sua casa!</h4>
+
+<br></br>
+<br></br>
+
+<h1 class="sobrenos">Nosso principal objetivo:</h1>
+<br></br>
+<h4 class="sobrenos">O cliente é a nossa principal motivação para a entrega de bons pratos. Portanto, nosso
+objetivo principal não poderia ser algo que não fosse você! Então, nós buscamos melhorar
+sempre, constantemente para que dentro ou fora do nosso restaurante você possa fazer
+grandes momentos com quem ama da melhor forma
+possível.</h4>
+
+<br></br>
+
+<h4 class="sobrenos">Buscamos sempre aprimorar nossos temperos para que o sabor seja cada vez
+melhor, focando também na entrega para que você receba seu pedido quentinho e
+cheirosos como se tivesse saído do forno direto para sua mesa. Pensamos sempre no seu
+paladar, como te deixar com aquele sentimento delicioso de se sentir em casa.</h4>
+
+
+
+</div>;
 };
 export default Contact;
